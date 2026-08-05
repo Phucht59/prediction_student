f402bd9cb6b9c0088e325cf4d7459565ad1a7858
Phucht59 <23dh112780@st.huflit.edu.vn>
2026-08-06 02:12:51 +0700

    feat(causal): export complete causal and imbalance toolkit

--- src/recommend_hybrid/causal/__init__.py
+++ src/recommend_hybrid/causal/__init__.py
@@ -5,6 +5,7 @@ landmark. It does not claim randomized-treatment or deployment effectiveness.
 """
 
 from .aipw import AIPWConfig, AIPWResult, CrossFittedAIPW
+from .bootstrap import ClusterBootstrapResult, cluster_bootstrap_mean
 from .diagnostics import (
     IdentifiabilityReport,
     IdentifiabilityThresholds,
@@ -12,6 +13,17 @@ from .diagnostics import (
     effective_sample_size,
     stabilized_iptw,
     standardized_mean_difference,
+)
+from .imbalance import (
+    IMBALANCE_MODES,
+    ImbalanceStudyResult,
+    run_frozen_embedding_imbalance_study,
+    select_validation_threshold,
+)
+from .pipeline import (
+    StageActionEvaluation,
+    StageActionTrialData,
+    StageAwareCausalEvaluator,
 )
 from .policy import (
     CausalActionDecision,
@@ -33,25 +45,44 @@ from .study_regularity import (
     study_regularity_components,
     study_regularity_score,
 )
+from .treatments import (
+    ACTION_TREATMENT_SPECS,
+    ActionTreatmentSpec,
+    FittedActionTreatmentRule,
+    fit_action_treatment_rule,
+)
 
 __all__ = [
+    "ACTION_TREATMENT_SPECS",
     "AIPWConfig",
     "AIPWResult",
+    "ActionTreatmentSpec",
     "CausalActionDecision",
     "CausalPolicyThresholds",
+    "ClusterBootstrapResult",
     "CrossFittedAIPW",
+    "FittedActionTreatmentRule",
+    "IMBALANCE_MODES",
     "IdentifiabilityReport",
     "IdentifiabilityThresholds",
+    "ImbalanceStudyResult",
     "LANDMARK_STAGES",
     "LandmarkStage",
     "RecommendationEvent",
     "STAGE_ORDER",
+    "StageActionEvaluation",
+    "StageActionTrialData",
+    "StageAwareCausalEvaluator",
     "StudyRegularityTreatmentDefinition",
     "TargetTrialProtocol",
     "assess_identifiability",
+    "cluster_bootstrap_mean",
     "effective_sample_size",
+    "fit_action_treatment_rule",
     "gate_causal_action",
     "resolve_recommendation_lifecycle",
+    "run_frozen_embedding_imbalance_study",
+    "select_validation_threshold",
     "stabilized_iptw",
     "stage_from_fraction",
     "standardized_mean_difference",
